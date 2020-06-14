71d3b08fada2e978b71dcd5fe28576320fbe3a9f
SyedaQurat <annieasad171994@gmail.com>
2020-06-14 20:22:24 +0500

    changes in conf.js and package.json

--- specs/conf.js
+++ specs/conf.js
@@ -11,8 +11,9 @@ exports.config =
     {
       //seleniumAddress: 'http://localhost:4444/wd/hub',
         capabilities: {
-            'browserName': 'chrome',
-            'chromeOptions': {
+            browserName: 'chrome',
+            chromeOnly: true,
+            chromeOptions: {
                 'args': ['no-sandbox']
             }
         },
