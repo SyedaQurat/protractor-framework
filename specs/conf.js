91e93d53c6159bcc26b6e0f6a5456fc72fee59a6
SyedaQurat <annieasad171994@gmail.com>
2020-06-14 14:29:46 +0500

    change of browserName in conf.js

--- specs/conf.js
+++ specs/conf.js
@@ -11,7 +11,7 @@ exports.config =
     {
       //seleniumAddress: 'http://localhost:4444/wd/hub',
         capabilities: {
-        browserName: 'chrome'
+        browserName: 'firefox'
       },
      directConnect: true,
 
