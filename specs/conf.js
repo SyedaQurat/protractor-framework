73569bab027caf5c013db069e10d631cf784fe8b
SyedaQurat <annieasad171994@gmail.com>
2020-06-14 18:18:54 +0500

    chromeOptions added in conf.js

--- specs/conf.js
+++ specs/conf.js
@@ -11,7 +11,10 @@ exports.config =
     {
       //seleniumAddress: 'http://localhost:4444/wd/hub',
         capabilities: {
-        browserName: 'chrome'
+        browserName: 'chrome',
+            'chromeOptions': {
+                args: ['--no-sandbox']
+            }
       },
      directConnect: true,
 
